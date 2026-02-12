b6721abc53a42d8904c5f524e460b339947dfe6e
Pankaj <159567776+pankaj90988@users.noreply.github.com>
2026-02-12 21:58:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,13 +51,13 @@ ADDING MIDDLEWARE TO HANDLE THE CORS POLICY ERROR /
 DESCRIBED THAT FROM WHICH ORIGIN BACKEND ACCEPT THE REQUEST AND RESPOND IT
 ================================
 """
-# origins = [
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173"
-# ]
+origins = [
+    "https://techfrontend-auth.onrender.com",
+    "http://127.0.0.1:5173"
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
